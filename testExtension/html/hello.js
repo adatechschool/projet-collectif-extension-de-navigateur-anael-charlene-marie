685ceaccfdb470fe9bea55213d751e8d04f055f5
Anaël <anael.toula@gmail.com>
2024-05-06 11:32:52 +0200

    mondayNew

--- testExtension/html/hello.js
+++ testExtension/html/hello.js
@@ -15,13 +15,14 @@ const apiKey ='854c7f80271d986efc886f7373e8d319';
         .then(data => {
             console.log(data);
            
-            document.querySelector('#city').innerHTML = `Hey !! How are youuu in ${data.name}?`;
-            document.querySelector('#temp').innerHTML = `The temperature is ${data.main.temp} °C`;
+            document.querySelector('#city').innerHTML = `Hey !! Comment ça va sur ${data.name}?`;
+            document.querySelector('#temp').innerHTML = `la température est de ${data.main.temp} °C`;
             console.log(data.main.temp)
-            document.querySelector('#humidity').innerHTML = `The weather is rather ${data.weather[0].main.description} Have a great day !.
+            document.querySelector('#humidity').innerHTML = `Le temps est plutôt ${data.weather[0].description}, bonne journée à toi !
              
             
-           Now you can choose to activate the theme if u want more fun ! <3 `;
+            
+            Maintenant tu peux choisir les thèmes pour plus de fun ! <3 `;
             // document.querySelector('#wind').innerHTML = `${data.wind.speed} km/h`;
             // document.querySelector('#icon').innerHTML = `${data.weather.icon}`
 
@@ -32,17 +33,59 @@ const apiKey ='854c7f80271d986efc886f7373e8d319';
                     //weatherObject est utilisé comme une variable de paramètre dans la fonction
                     // weatherArray += weatherObject.main + ", ";
 
-                    
-             
-
-            
-             
-        
-            
-            
-            
-            
-        })
+                     })
         .catch(err => console.log('Erreur : ' + err));
     }
 })
+
+// let page1 = document.getElementById("page1");
+// let page2 = document.getElementById("page2");
+// let meteoSearch = document.getElementById("meteoSearch");
+// let back = document.getElementById("bacK");
+//  meteoSearch.addEventListener("click", () => {
+//   if(getComputedStyle(page1).display != "none"){
+//     page1.style.display = "none";
+//   } else {
+//     page1.style.display = "block";
+//   }
+// })
+
+// function returnToPage1() {
+//   document.getElementById("page1").style.display = "block";
+//   document.getElementById("page2").style.display = "none";
+//   // return false; // Pour empêcher le navigateur de suivre le lien
+// }
+let page1 = document.getElementById("page1");
+let page2 = document.getElementById("page2");
+let meteoSearch = document.getElementById("meteoSearch");
+let back = document.getElementById(".back"); 
+
+meteoSearch.addEventListener("click", () => {
+    if (getComputedStyle(page1).display != "none") {
+        page1.style.display = "none";
+        page2.style.display = "block"; // Affiche la page 2 lorsque la recherche est effectuée
+    } else {
+        // Ici, vous devez ajouter le code pour traiter la recherche météo
+    }
+});
+
+// variable pour garder l'id de la page1
+let currentPageId = "page1";
+
+// Ajoute un écouteur d'événement clic au lien "back"
+document.querySelector('.back').addEventListener('click', returnToPreviousPage);
+
+// Fonction pour revenir à la page/div précédente
+function returnToPreviousPage() {
+    let currentPage = document.getElementById(currentPageId);
+    currentPage.style.display = "none"; // Masque la page/div courante
+
+    // Détermine l'ID de la page/div précédente
+    let previousPageId = currentPageId === "page1" ? "page2" : "page1";
+    let previousPage = document.getElementById(previousPageId);
+    previousPage.style.display = "block"; // Affiche la page/div précédente
+
+    // Met à jour l'ID de la page/div actuelle
+    currentPageId = previousPageId;
+}
+
